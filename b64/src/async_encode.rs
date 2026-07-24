002677981ecc2bacab6336f7999a3293b929b6ab
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-07-24 14:51:19 +0200

    DRY up async streaming encoders with generic helper

--- b64/src/async_encode.rs
+++ b64/src/async_encode.rs
@@ -2,17 +2,15 @@ use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use crate::encode::Encoder;
 
-/// Async Base64 encoding using Tokio AsyncRead/AsyncWrite.
-pub async fn encode_reader_to_writer_async<R, W>(
+async fn encode_reader_to_writer_async_generic<R, W>(
+    mut enc: Encoder,
     reader: &mut R,
     writer: &mut W,
-    wrap: Option<usize>,
 ) -> tokio::io::Result<()>
 where
     R: AsyncRead + Unpin,
     W: AsyncWrite + Unpin,
 {
-    let mut enc = Encoder::new(wrap);
     let mut buf = [0u8; 4096];
     let mut out = String::with_capacity(8192);
 
@@ -35,6 +33,20 @@ where
     Ok(())
 }
 
+/// Async Base64 encoding using Tokio AsyncRead/AsyncWrite.
+pub async fn encode_reader_to_writer_async<R, W>(
+    reader: &mut R,
+    writer: &mut W,
+    wrap: Option<usize>,
+) -> tokio::io::Result<()>
+where
+    R: AsyncRead + Unpin,
+    W: AsyncWrite + Unpin,
+{
+    let enc = Encoder::new(wrap);
+    encode_reader_to_writer_async_generic(enc, reader, writer).await
+}
+
 /// URL-safe async encoder.
 pub async fn encode_url_safe_reader_to_writer_async<R, W>(
     reader: &mut R,
@@ -45,25 +57,6 @@ where
     R: AsyncRead + Unpin,
     W: AsyncWrite + Unpin,
 {
-    let mut enc = Encoder::new_url_safe(wrap);
-    let mut buf = [0u8; 4096];
-    let mut out = String::with_capacity(8192);
-
-    loop {
-        let n = reader.read(&mut buf).await?;
-        if n == 0 {
-            break;
-        }
-
-        enc.encode_block(&buf[..n], &mut out);
-
-        if out.len() >= 4096 {
-            writer.write_all(out.as_bytes()).await?;
-            out.clear();
-        }
-    }
-
-    enc.encode_end(&mut out);
-    writer.write_all(out.as_bytes()).await?;
-    Ok(())
+    let enc = Encoder::new_url_safe(wrap);
+    encode_reader_to_writer_async_generic(enc, reader, writer).await
 }
